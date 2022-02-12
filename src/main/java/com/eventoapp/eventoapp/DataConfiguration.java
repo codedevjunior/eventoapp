5dfe4510b9ccde8aa4a04cd014255258b90417d9
codedevjunior <code.devjunior@outlook.com.br>
2022-02-12 17:35:29 -0300

    Criando e configurando um banco de dados MySQL

---
+++ src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -0,0 +1,11 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class DataConfiguration {
+
+   //continuar na aula3
+}
